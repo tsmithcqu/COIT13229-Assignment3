41933ebae5c37133f00746b091de9e0f19762c2c
Carl <carlosmendeztafe@gmail.com>
2024-05-08 00:44:08 +1000

    Create ProductService.java

---
+++ ProductService.java
@@ -0,0 +1,9 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ProductService {
+    public static void saveProductsFromCSV(String filePath) {
+        // Read from A3-products.csv file and save product details to the MySql Database
+    }
+}
