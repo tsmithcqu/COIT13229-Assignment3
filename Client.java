55ea869be5e5ca500a7c853989b252129f2e89f0
Carl <carlosmendeztafe@gmail.com>
2024-05-08 00:50:54 +1000

    Create Client.java

---
+++ Client.java
@@ -0,0 +1,22 @@
+import java.net.Socket;
+
+public class Client {
+    private Socket socket;
+
+    public Client(String serverAddress, int port) {
+        // Initialize socket connection
+    }
+
+    public void sendRequest(Object request) {
+        // Send requests to the server
+    }
+
+    public Object receiveResponse() {
+        // Receive responses from the server
+        return null;
+    }
+
+    public void encryptPassword(String password) {
+        // Encrypt the password using the server's public key
+    }
+}
