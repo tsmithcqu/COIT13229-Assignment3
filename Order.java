2c7b26fd7e91ca1087ef2540e0d2976e7f635427
Carl <carlosmendeztafe@gmail.com>
2024-05-08 01:00:03 +1000

    Create Order.java

---
+++ Order.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Order {
+    private Customer customer;
+    private List<Product> products;
+    private double totalPrice;
+
+    public Order(Customer customer) {
+        this.customer = customer;
+        this.products = new ArrayList<>();
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void addProduct(Product product) {
+        this.products.add(product);
+        calculateTotalPrice();
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    private void calculateTotalPrice() {
+        totalPrice = 0;
+        for (Product product : products) {
+            totalPrice += product.getUnitPrice() * product.getQuantity();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "customer=" + customer +
+                ", products=" + products +
+                ", totalPrice=" + totalPrice +
+                '}';
+    }
+}
