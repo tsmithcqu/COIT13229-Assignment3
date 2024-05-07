18b5a43067cc784056860688ab5c8e03b0eea746
Carl <carlosmendeztafe@gmail.com>
2024-05-08 01:00:55 +1000

    Create DeliverySchedule.java

---
+++ DeliverySchedule.java
@@ -0,0 +1,45 @@
+public class DeliverySchedule {
+    private String postcode;
+    private String deliveryDay;
+    private double deliveryCost;
+
+    public DeliverySchedule(String postcode, String deliveryDay, double deliveryCost) {
+        this.postcode = postcode;
+        this.deliveryDay = deliveryDay;
+        this.deliveryCost = deliveryCost;
+    }
+
+    // Getters and setters
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getDeliveryDay() {
+        return deliveryDay;
+    }
+
+    public void setDeliveryDay(String deliveryDay) {
+        this.deliveryDay = deliveryDay;
+    }
+
+    public double getDeliveryCost() {
+        return deliveryCost;
+    }
+
+    public void setDeliveryCost(double deliveryCost) {
+        this.deliveryCost = deliveryCost;
+    }
+
+    @Override
+    public String toString() {
+        return "DeliverySchedule{" +
+                "postcode='" + postcode + '\'' +
+                ", deliveryDay='" + deliveryDay + '\'' +
+                ", deliveryCost=" + deliveryCost +
+                '}';
+    }
+}
