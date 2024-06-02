ed1a069666cec45e894e02b6e8f80ec0002ce92f
tsmithcqu <130130661+tsmithcqu@users.noreply.github.com>
2024-06-02 16:14:35 +1000

    Update CustomerViewAdmin.java

--- CustomerViewAdmin.java
+++ CustomerViewAdmin.java
@@ -1,1 +1,26 @@
+package mhds;
 
+import javax.swing.*;
+import java.awt.*;
+
+public class CustomerViewAdmin extends JFrame {
+    private JTextField nameField, emailField, passwordField, addressField, phoneNumberField;
+    private JButton addButton;
+    private Client client;
+
+    public CustomerViewAdmin() {
+        super("Register New Customer");
+        this.client = new Client();
+        createForm();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300, 300);
+        setLocationRelativeTo(null);
+    }
+
+    private void createForm() {
+      // To do
+    }
+
+    private void submitCustomerData() {
+      // To Do
+    }
