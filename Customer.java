92f7e7a159417d9b22b608482b09e5307538ae36
tsmithcqu <130130661+tsmithcqu@users.noreply.github.com>
2024-06-03 13:17:29 +1000

    Added comments to code.

--- Customer.java
+++ Customer.java
@@ -2,19 +2,29 @@ package mhds;
 
 import java.io.Serializable;
 
-public class Customer {
-    private String name;
-    private String email;
-    private String password;
-    private String address;
-    private String phoneNumber;
+/**
+* The Customer class models the data for a customer and is designed to be passed between client and server.
+*/
+public class Customer implements Serializable {
 
+    /**
+    * Private fields to hold customer information.
+    */
+    private String name;        // Customer's name.
+    private String email;       // Customer's email address.
+    private String password;    // Customer's password. This is in plaintext - need to work out how to encrypt this. 
+    private String address;     // Customer's physical address.
+    private String phoneNumber; // Customer's phone number.
+
+    /**
+    * Constructor to initialise a new customer with all attributes.
+    */
     public Customer(String name, String email, String password, String address, String phoneNumber) {
-        this.fullName = name;
-        this.phoneNumber = email;
-        this.email = password;
-        this.password = address;
-        this.address = phoneNumber;
+        this.name = name;               // Initialise the name field.
+        this.email = email;             // Initialise the email field.
+        this.password = password;       // Initialise the password field.
+        this.address = address;         // Initialise the address field.
+        this.phoneNumber = phoneNumber; // Initialise the phone number field.
     }
 
     // Getters and setters
