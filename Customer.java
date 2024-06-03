69e94534c8b8b954047ce187bdac7dc9f671307d
tsmithcqu <130130661+tsmithcqu@users.noreply.github.com>
2024-06-03 13:22:31 +1000

    Adding comments and removing setters as they are not required due to users in the database not requiring to be changed once added.

--- Customer.java
+++ Customer.java
@@ -27,44 +27,22 @@ public class Customer implements Serializable {
         this.phoneNumber = phoneNumber; // Initialise the phone number field.
     }
 
-    // Getters and setters
-    public String name() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
+     /**
+     * Getter methods for each of the customer's attributes.
+     */
+    public String getName() {
+        return name; // Return the customer's name.
     }
 
     public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
+        return email; // Return the customer's email.
     }
 
     public String getPassword() {
-        return password;
+        return password; // Return the customer's password.
     }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
+    
     public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
+        return phoneNumber; // Return the customer's phone number.
     }
 }
