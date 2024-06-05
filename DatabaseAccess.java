47ccc7b74d641368188bd1cce0b6d67f2b63e5d7
tsmithcqu <130130661+tsmithcqu@users.noreply.github.com>
2024-06-05 21:00:18 +1000

    Update DatabaseAccess.java

--- DatabaseAccess.java
+++ DatabaseAccess.java
@@ -1,9 +1,8 @@
 package mhds;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 
 
