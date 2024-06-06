3b3604b4e4d650987044b3b61d37110d1676d98d
tsmithcqu <130130661+tsmithcqu@users.noreply.github.com>
2024-06-06 16:49:08 +1000

    Update OrderViewAdmin.java

--- OrderViewAdmin.java
+++ OrderViewAdmin.java
@@ -1,6 +1,7 @@
 
 /**
 Old code created by Carlos. May need to redo. 
+Not currently in use. 
 
 
 import javax.swing.*;
@@ -30,33 +31,6 @@ public class OrderViewAdmin extends JFrame {
         add(panel);
     }
 
-    private void loadOrderData(JTable orderTable) {
-        try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mhds", "root", "password");
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM orders");
-
-            String[] columnNames = {"Order ID", "Customer Email", "Product ID", "Quantity", "Total Price"};
-            Object[][] data = new Object[50][5];
-            int rowCount = 0;
-
-            while (resultSet.next()) {
-                data[rowCount][0] = resultSet.getInt("order_id");
-                data[rowCount][1] = resultSet.getString("customer_email");
-                data[rowCount][2] = resultSet.getString("product_id");
-                data[rowCount][3] = resultSet.getInt("quantity");
-                data[rowCount][4] = resultSet.getDouble("total_price");
-                rowCount++;
-            }
-
-            orderTable.setModel(new javax.swing.table.DefaultTableModel(data, columnNames));
-            resultSet.close();
-            statement.close();
-            connection.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
